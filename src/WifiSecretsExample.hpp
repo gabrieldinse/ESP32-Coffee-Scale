7d927bb28a4c3aa410a307170eee0342b0f5a24e
Gabriel Dinse <gabriel.dinse@outlook.com>
2025-12-03 11:57:00 -0300

    Initial wifi connectivity

---
+++ src/WifiSecretsExample.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// Copy this file to WifiSecrets.hpp and fill in your WiFi credentials
+
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASSWORD "YourPassword"
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
-#include "HX711.h"
 #include <Arduino.h>
+#include <WiFi.h>
+
+#include "HX711.h"
+
+#include "WifiSecrets.hpp" // See WifiSecretsExample.hpp
 
 // HX711 circuit wiring
-#define LOADCELL_DOUT_PIN 21  // D21 pin on board
-#define LOADCELL_SCK_PIN 22   // D22 pin on board
+#define LOADCELL_DOUT_PIN 16  // D16 pin on board
+#define LOADCELL_SCK_PIN 4    // D4 pin on board
 
 // 2. Adjustment settings
 // Coke: 380g grams
@@ -22,10 +26,35 @@ void setup() {
   loadcell.set_scale(LOADCELL_DIVIDER);
   loadcell.tare(20);
   Serial.println("Insert the item to be weighed"); 
+
+  // Wifi
+  WiFi.mode(WIFI_STA);  // Station mode
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // Wait for connection
+  int max_retries = 20;
+  while (WiFi.status() != WL_CONNECTED && max_retries-- > 0) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to WiFi");
+  }
 }
  
 void loop() {
   Serial.print("Reading: ");
   Serial.println(loadcell.get_units(10), 0);
   delay(100);
+
+  static uint64_t last = 0;
+  if (millis() - last > 5000) {
+    last = millis();
+    Serial.println(WiFi.status() == WL_CONNECTED ? "Still connected" : "Disconnected");
+  }
 }
