561560d5c8090cf5996a6aaab49d71f294ff35bc
Lucas Cunha <lucas.cunha@zpesystems.com>
2025-12-03 11:58:15 -0300

    Display scale reding on LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "HX711.h"
+#include <LCD_I2C.h>
 #include <Arduino.h>
 
 // HX711 circuit wiring
-#define LOADCELL_DOUT_PIN 21  // D21 pin on board
-#define LOADCELL_SCK_PIN 22   // D22 pin on board
+#define LOADCELL_DOUT_PIN 16  // D16 pin on board
+#define LOADCELL_SCK_PIN 4   // D4 pin on board
 
 // 2. Adjustment settings
 // Coke: 380g grams
@@ -12,7 +13,8 @@
 const float LOADCELL_DIVIDER = static_cast<float>(152950) / 380;
 
 HX711 loadcell;
- 
+LCD_I2C lcd(0x27, 16, 2);
+
 void setup() {
 
   Serial.begin(9600);
@@ -21,11 +23,20 @@ void setup() {
   loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  // ✅ REQUIRED !!!
   loadcell.set_scale(LOADCELL_DIVIDER);
   loadcell.tare(20);
-  Serial.println("Insert the item to be weighed"); 
+  Serial.println("Insert the item to be weighed");
+
+  // Display testing
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("Coffee Scale");
 }
  
 void loop() {
+  int reading = loadcell.get_units(10);
   Serial.print("Reading: ");
-  Serial.println(loadcell.get_units(10), 0);
+  Serial.println(reading, 0);
+  lcd.setCursor(0, 1);
+  lcd.print("Weight: ");
+  lcd.print(reading, 0);
   delay(100);
 }
